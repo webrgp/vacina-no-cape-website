8526c91a7647d9bf654a533d778dcaf70a6b6343
Rodrigo Passos <rodrigo@hellodative.com>
2021-03-13 11:14:59 -0500

    add assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#08518E`,
         theme_color: `#08518E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/apple-touch-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
