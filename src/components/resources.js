a5f2a602a8eaab2e03dbd341ab01b06b4a688b22
Rodrigo Passos <rodrigo@hellodative.com>
2021-03-14 10:52:12 -0400

    reorder resource

--- src/components/resources.js
+++ src/components/resources.js
@@ -11,10 +11,10 @@ const Resources = () => {
       </header>
       <div tw="max-w-screen-lg  mx-auto mt-2 bg-white p-4">
         <ul>
+          <li><strong><Link to="/faq" tw="text-blue-500 hover:underline">Perguntas Frequentes</Link></strong></li>
           <li><a href="/COVID19vaccine_Preparing-for-Appointment_Final_PT-BR.jpg" target="_blank" rel="noreferrer" tw="text-blue-500 hover:underline">Como se preparar para a vacina contra o COVID-19</a></li>
           <li><a href="/Vaccine_Need-to-Know_Final_Updated-2021-02-12_PT-BR.jpg" target="_blank" rel="noreferrer" tw="text-blue-500 hover:underline">Detalhes que você precisa saber sobre a vacina contra o COVID-19</a></li>
           <li><a href="/Vaccine_Post_Vaccine_FAQ_FINAL_020521_PT-BR.jpg" target="_blank" rel="noreferrer" tw="text-blue-500 hover:underline">Perguntas frequentes após receber a vacina contra o COVID-19</a></li>
-          <li><Link to="/faq" tw="text-blue-500 hover:underline">Perguntas Frequentes</Link></li>
         </ul>
       </div>
     </div>
