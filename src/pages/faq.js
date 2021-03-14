aeacb1c1db48fe53bc9f8460ec83828e2ee06d23
Rodrigo Passos <rodrigo@hellodative.com>
2021-03-14 11:31:52 -0400

    update FAQ

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -52,29 +52,47 @@ const IndexPage = () => (
         <dl>
           <QA question="O que posso esperar na minha consulta para tomar a vacina contra COVID-19?">Acesse <a href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/expect.html" target="_blank" rel="noreferrer" tw="text-blue-500 hover:underline">O que posso esperar na minha consulta para tomar a vacina contra COVID-19 | CDC</a> para ver dicas do que você pode esperar quando for vacinado, quais informações seu provedor dará a você, e os recursos que poderá usar para monitorar sua saúde depois de ser vacinado.</QA>
 
-          <QA question=""></QA>
+          <QA question="Preciso usar máscara quando for tomar a vacina contra COVID-19?">Sim. O CDC recomenda que durante a pandemia as pessoas <strong>usem uma máscara</strong> que cubra seu nariz e boca quando entrarem em contato com outras pessoas que não as pessoas de sua casa, quando estiverem em unidades de saúde e quando forem tomar qualquer vacina, incluindo a vacina contra COVID-19. Qualquer pessoa que tenha problemas para respirar ou não consiga remover a máscara sem ajuda não deve usar máscara.</QA>
 
-          <QA question=""></QA>
+          <QA question="Posso escolher qual vacina irei tomar?">Não. Os recipientes da vacina receberão a vacina oferecida a eles quando comparecerem a uma clínica de vacinação. As duas vacinas são altamente eficazes e seguras.</QA>
 
-          <QA question=""></QA>
+          <QA question="Quanto tempo tenho de esperar entre a primeira e a segunda dose da vacina contra COVID- 19?">A vacina da Pfizer requer que as duas doses sejam administradas em um intervalo mínimo de 21 dias. A vacina da Moderna requer que as duas doses sejam administradas em um intervalo mínimo de 28 dias.</QA>
 
-          <QA question=""></QA>
-
-          <QA question=""></QA>
-
-          <QA question=""></QA>
-
-          <QA question=""></QA>
-
-          <QA question=""></QA>
-
-          <QA question=""></QA>
-
-          <QA question=""></QA>
-
-          <QA question=""></QA>
+          <QA question="Receberei alguma coisa para comprovar que tomei a vacina?">Você deve receber um cartão de vacinação ou uma folha impressa que informa qual vacina contra COVID-19 você tomou, a data em que a tomou e onde. Você deve receber uma versão impressa ou eletrônica de uma ficha técnica que fornece mais informações sobre a vacina contra COVID-19 específica que está sendo oferecida a você. Cada vacina autorizada contra COVID-19 tem sua própria ficha técnica que contém informações para ajudá-lo a entender os riscos e benefícios de receber aquela vacina específica.</QA>
 
         </dl>
+      </section>
+
+      <section>
+        <header><h3 tw="text-2xl font-semibold my-4">Depois de tomar a vacina</h3></header>
+
+        <QA question="O que devo fazer se eu sentir algum sintoma depois de tomar a vacina contra COVID-19, como febre e dores no corpo?">
+          Se você sentir alguma dor ou um desconforto significativo, converse com seu médico, que pode recomendar medicamentos vendidos sem receita médica, como ibuprofeno ou acetaminofeno (paracetamol). Para reduzir a dor e o desconforto no local da injeção, aplique uma toalha limpa, fria e úmida sobre a área e use ou exercite o braço. Para reduzir o desconforto causado pela febre, beba muitos líquidos e vista roupas leves. Na maioria dos casos, o desconforto causado pela febre ou dor é normal, mas entre em contato com seu médico se:
+
+          <ul>
+            <li>a vermelhidão ou sensibilidade no local onde você recebeu a injeção aumentar depois de 24 horas</li>
+            <li>os efeitos colaterais estiverem preocupando você ou não desaparecem depois de alguns dias</li>
+          </ul>
+        </QA>
+
+        <QA question="Como posso informar se eu tiver algum efeito colateral depois de tomar a vacina contra COVID-19?"><strong>V-safe</strong> é uma ferramenta de smartphone que usa mensagens de texto e pesquisas na web para fornecer verificações de saúde personalizadas, depois de você tomar uma vacina contra COVID- 19. Usando o V-safe, você pode informar rapidamente ao CDC se apresentar algum efeito colateral depois de tomar a vacina contra COVID-19. Dependendo de suas respostas, alguém do CDC poderá ligar para verificar como você está e obter mais informações. E o V-safe irá lembrá- lo de tomar sua segunda dose da vacina contra COVID-19, se precisar. Para inscrever-se no v- safe, acesse <a href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/safety/vsafe.html" target="_blank" rel="noreferrer" tw="text-blue-500 hover:underline">Verificador V-safe da saúde depois da vacinação | CDC.</a></QA>
+
+        <QA question="Quanto tempo demora, depois da administração da vacina contra COVID-19, para que ela tenha efeito?">Geralmente, leva algumas semanas para o corpo desenvolver imunidade após a vacinação. Isso significa que é possível que uma pessoa seja infectada com o vírus que causa a COVID-19 logo antes ou logo depois da vacinação e fique doente. Isso acontece porque a vacina não teve tempo suficiente para proporcionar proteção.</QA>
+
+        <QA question="Quanto tempo dura a proteção das vacinas contra COVID-19?">Ainda não temos dados para dizer por quanto tempo as vacinas contra COVID-19 fornecerão proteção contra a doença, mesmo se você pegar COVID-19. Os especialistas continuam a realizar mais estudos para descobrir se as vacinas também impedem que pessoas espalhem a COVID-19.</QA>
+
+        <QA question="A imunidade depois de uma pessoa pegar COVID-19 dura mais do que a proteção oferecida pelas vacinas contra COVID-19?">A proteção que alguém adquire por ter uma infecção (chamada de imunidade natural) varia dependendo da doença e varia de pessoa para pessoa. Como esse vírus é novo, não sabemos quanto tempo a imunidade natural poderá durar. Algumas evidências iniciais - baseadas em algumas pessoas - parecem sugerir que a imunidade natural pode não durar muito. Em relação à vacinação, não saberemos quanto tempo a imunidade dura até que tenhamos uma vacina e mais dados sobre a eficácia da vacina. Tanto a imunidade natural quanto a imunidade induzida por vacina são aspectos importantes da COVID-19 sobre os quais os especialistas estão tentando aprender mais, e o CDC manterá o público informado, à medida que novas evidências sejam disponibilizadas.  </QA>
+
+        <QA question="Qual é a eficácia de uma dose da vacina contra COVID-19 em comparação com duas doses?">As vacinas contra COVID-19 não foram estudadas para uso com uma única dose. As pessoas devem receber as duas doses da vacina para estarem totalmente vacinadas e para que a vacina seja eficaz.</QA>
+
+        <QA question="Tenho de obedecer às Ordens de viagem se já tomei a vacina contra COVID-19?">Sim. Neste momento, as pessoas vacinadas devem continuar a cumprir a Ordem de Viagem do Governador e os requisitos de teste e quarentena relacionados. É importante que todos continuem usando todas as medidas de saúde pública disponíveis para ajudar a acabar com esta pandemia. Cubra sua boca e nariz com uma máscara quando estiver perto de outras pessoas, fique a pelo menos a 6 pés (2 m) de distância de outras pessoas, evite aglomerações e lave as mãos com frequência.</QA>
+
+        <QA question="Ainda terei de ficar de quarentena depois de ter sido vacinado, se for identificado como um contato próximo de alguém com COVID-19?">Sim. Ainda não temos dados que indiquem por quanto tempo as vacinas contra COVID-19 fornecem proteção para não ficar doente nem se as vacinas também evitam que as pessoas transmitam a COVID-19. Os especialistas precisam entender mais a proteção que as vacinas contra COVID-19 fornecem antes de fazerem alterações nas recomendações de quarentena da saúde pública. Outros fatores, inclusive quantas pessoas são vacinadas, e como o vírus está se espalhando nas comunidades, também afetarão essa decisão.</QA>
+
+        <QA question="Preciso usar máscara e evitar contato próximo com outras pessoas se já recebi as duas doses da vacina?">Sim. Enquanto os especialistas aprendem mais sobre a proteção que as vacinas contra COVID- 19 fornecem em condições da vida real, é importante que todos continuem usando todas as ferramentas disponíveis para nos ajudar a parar esta pandemia, como usar uma máscara que cubra sua boca e nariz, lavar as mãos com frequência e permanecer a pelo menos 6 pés (2 m) de distância de outras pessoas. Juntos, a vacinação contra a COVID-19 e seguir as recomendações do CDC quanto a como se proteger e proteger a outras pessoas oferecerão a melhor proteção contra o contágio e a disseminação da COVID-19. Os especialistas precisam entender mais sobre a proteção que as vacinas contra COVID-19 fornecem antes de decidirem alterar as recomendações sobre as medidas que todos nós devem tomar para retardar a disseminação do vírus que causa COVID-19. Outros fatores, inclusive quantas pessoas são vacinadas e como o vírus está se espalhando nas comunidades, também afetarão essa decisão.</QA>
+
+        <QA question="Quando poderei parar de usar máscara e evitar contato próximo com outras pessoas depois de ter sido vacinado?">Não há informações suficientes atualmente disponíveis para dizer se ou quando o CDC irá parar de recomendar às pessoas que usem máscara e evitem contato próximo com outras pessoas para ajudar a prevenir a disseminação do vírus que causa a COVID-19. Os especialistas precisam entender mais a proteção que as vacinas contra COVID-19 fornecem antes de tomar essa decisão. Outros fatores, inclusive quantas pessoas são vacinadas e como o vírus está se espalhando nas comunidades, também afetarão essa decisão.</QA>
+
       </section>
     </div>
 
